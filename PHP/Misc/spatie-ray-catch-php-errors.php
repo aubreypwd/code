de5d75094e6302f258ddf6ad28c6269bb131adb5
Aubrey Portwood <code@aubreypwd.com>
2021-12-27 10:49:12 -0700

    Add my impovements.

--- PHP/Misc/spatie-ray-catch-php-errors.php
+++ PHP/Misc/spatie-ray-catch-php-errors.php
@@ -17,18 +17,54 @@ function ____error_to_spatie_ray() {
 		return;
 	}
 
-	// Comment these out to ignore notices and warnings.
+	// Comment these out to ignore errors.
 	if ( in_array( true, [
-		// $error['type'] === E_WARNING,
-		// $error['type'] === E_NOTICE,
-		// $error['type'] === E_CORE_WARNING,
-		// $error['type'] === E_COMPILE_WARNING,
-		// $error['type'] === E_USER_WARNING,
-		// $error['type'] === E_USER_NOTICE,
+
+		/* Ignore Notices, Warnings, and Deprecations */
+
+			$error['type'] === E_WARNING,
+			$error['type'] === E_NOTICE,
+			// $error['type'] === E_CORE_WARNING,
+			// $error['type'] === E_COMPILE_WARNING,
+			// $error['type'] === E_USER_WARNING,
+			// $error['type'] === E_USER_NOTICE,
+			// $error['type'] === E_STRICT,
+			// $error['type'] === E_RECOVERABLE_ERROR,
+			// $error['type'] === E_DEPRECATED,
+			// $error['type'] === E_USER_DEPRECATED,
+
+		/* Ignore Errors */
+
+			// $error['type'] === E_ERROR,
+			// $error['type'] === E_PARSE,
+			// $error['type'] === E_CORE_ERROR,
+			// $error['type'] === E_COMPILE_ERROR,
+			// $error['type'] === E_USER_ERROR,
+
 	], true ) ) {
+
+		// Don't show anything in Ray about these.
 		return;
 	}
 
-	\ray( $error );
+	// If we hit any of theses ERROR's, we'll show the app so you know.
+	if ( in_array( true, [
+
+		/* Errors */
+
+		$error['type'] === E_ERROR,
+		$error['type'] === E_PARSE,
+		$error['type'] === E_CORE_ERROR,
+		$error['type'] === E_COMPILE_ERROR,
+		$error['type'] === E_USER_ERROR,
+
+	], true ) ) {
+
+		// Log the error (in red) and show Ray.
+		\ray( $error )->showApp()->red();
+		return;
+	}
+
+	\ray( $error ); // Just log the error to Ray (in red).
 }
 register_shutdown_function( '____error_to_spatie_ray' );
