7057dfb3b7032b44fd1557f4da90c7a44ef4cf1d
Aubrey Portwood <code@aubreypwd.com>
2021-12-27 10:01:32 -0700

    Improve with the ability to ignore notices and warnings.

--- PHP/Misc/spatie-ray-catch-php-errors.php
+++ PHP/Misc/spatie-ray-catch-php-errors.php
@@ -17,6 +17,18 @@ function ____error_to_spatie_ray() {
 		return;
 	}
 
+	// Comment these out to ignore notices and warnings.
+	if ( in_array( true, [
+		// $error['type'] === E_WARNING,
+		// $error['type'] === E_NOTICE,
+		// $error['type'] === E_CORE_WARNING,
+		// $error['type'] === E_COMPILE_WARNING,
+		// $error['type'] === E_USER_WARNING,
+		// $error['type'] === E_USER_NOTICE,
+	], true ) ) {
+		return;
+	}
+
 	\ray( $error );
 }
 register_shutdown_function( '____error_to_spatie_ray' );
