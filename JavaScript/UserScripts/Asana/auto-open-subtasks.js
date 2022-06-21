356a67c6d8bbf9c684111eecc3b1eddbdb9be26f
Aubrey Portwood <code@aubreypwd.com>
2022-06-21 10:31:44 -0600

    Use the UserScript frontmatter for notes.

--- JavaScript/UserScripts/Asana/auto-open-subtasks.js
+++ JavaScript/UserScripts/Asana/auto-open-subtasks.js
@@ -1,23 +1,9 @@
-/**
- * Automatically Open Subtasks in Asana
- *
- * Note, if you use WebCatalog, just add this to the JS code
- * injection, it works.
- *
- * Also this version checks for links to click indefinitly, vs how
- * the original only ran for 10 seconds.
- *
- * @since Tuesday, June 21, 2022
- *        - Adjustments to check indefinitly every 2 seconds.
- *
- * @see   https://forum.asana.com/t/allow-to-load-all-sub-tasks-at-once/43620/12 Source
- */
-
 // ==UserScript==
-// @name         Auto-click "Load More Subtasks"
+// @name         Auto-click "Load More Subtasks" in Asana
 // @version      1.1
 // @description  Automatically clicks the "Load More Subtasks" link in Asana when present.
-// @author       Aron Beal
+//               Note, if you are using WebCatalog, just add this to the JS injection panel in the settings.
+// @author       Aubrey Portwood; a modified version by Aron Beal
 // @match        https://app.asana.com/*
 // @grant        none
 (function () {
