55a970a770b583819b38f7acfaa1f9c859d46db0
Aubrey Portwood <code@aubreypwd.com>
2022-11-22 13:49:35 -0700

    Add my configuration script for xDebug/LocalWP.

---
+++ PHP/LocalWP/xdebug-consistent-config.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Ensure your LocalWP (https://localwp.com) xDebug configuration is consistent.
+ *
+ * Change to your configuration and place this in your wp-config.php file.
+ *
+ * This ensures that:
+ *
+ * 1. The port for both v2 and v3 of xDebug is set to 9021
+ * 2. For xDebug v2 xdebug.remote_autostart is turned On
+ * 3. For xDebug v3 xdebug.start_with_request is set to yes vs trigger
+ *
+ * This will go through all your php.ini.hbs files in conf and set xDebug up
+ * this way.
+ *
+ * The reason I use this is because when you switch PHP versions Local doesn't keep your
+ * configurations (mainly autostart and the port), it can turn it off and reset the
+ * port to 9003 which can conflict with your local php.
+ *
+ * @since Nov 22, 2022
+ * @author Aubrey Portwood <code@aubreypwd.com>
+ */
+
+// Make sure PHP.ini xDebug configuration is the way you like it.
+foreach ( glob( dirname( dirname( __DIR__ ) ) . "/conf/php*", GLOB_BRACE ) as $php_dir ) {
+
+	$php_ini = "{$php_dir}/php.ini.hbs";
+
+	if ( ! file_exists( $php_ini ) ) {
+		continue;
+	}
+
+	$contents = file_get_contents( $php_ini );
+
+	// Newer PHP.
+	$contents = str_replace(
+		array(
+			'xdebug.client_port=9003', // Set your preferred port here.
+			'xdebug.start_with_request=trigger',
+		),
+		array(
+			'xdebug.client_port=9021',
+			'xdebug.start_with_request=yes',
+		),
+		$contents
+	);
+
+	// Older PHP.
+	$contents = str_replace(
+		array(
+			'xdebug.remote_connect_back=Off',
+			'xdebug.remote_port="9000"',
+		),
+		array(
+			"xdebug.remote_connect_back=On\nxdebug.remote_autostart=1",
+			'xdebug.remote_port="9021"', // Set your preferred port here.
+		),
+		$contents
+	);
+
+	file_put_contents( $php_ini, $contents );
+}
